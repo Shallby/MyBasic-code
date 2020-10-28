7af1c2c14112ad0af4fa13cbff42b07072d785ee
Shallby <1012437497@qq.com>
2020-10-28 13:53:27 +0800

    This java class is to creat a Function to print one array ,what's more,I have overload the method.

---
+++ day07-code/src/come/atseu/java/ArrayForPrint.java
@@ -0,0 +1,35 @@
+package come.atseu.java;
+
+import javax.print.attribute.standard.MediaPrintableArea;
+import java.lang.reflect.Array;
+
+/*
+运用方法来打印数组
+方法的输入可以是数组
+ */
+public class ArrayForPrint {
+    public static void main(String[] args) {
+        int[] Array={23,24,53,123};
+        String[] Array2={"我是一只小小鸟","想要飞的老高高"};
+        printArray(Array);
+        System.out.println("刚刚调用了数组的方法打印");
+        printArray(Array2);
+        System.out.println("刚刚重载了方法，并且调用了 字符串的打印");
+    }
+    //打印一个int型的数组
+    public static void printArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("元素"+i+":"+array[i]+"   ");
+        }
+        System.out.println("");
+    }
+
+    //涉及到方法的重载
+    public static void printArray(String[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("元素"+i+":"+array[i]+"   ");
+        }
+        System.out.println("");
+    }
+
+}
