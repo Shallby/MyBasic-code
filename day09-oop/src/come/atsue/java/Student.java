9057753f6c652f814f0b5ccc3e589829163492a3
Shallby <1012437497@qq.com>
2020-10-29 15:31:38 +0800

    Using the constructor we defined,should be careful with the general method.cause of the construct method doesn't have return,So we shouldn't use a parameter to get the result of construction function.

--- day09-oop/src/come/atsue/java/Student.java
+++ day09-oop/src/come/atsue/java/Student.java
@@ -8,7 +8,8 @@ package come.atsue.java;
     }
 注意：1.构造方法的名称必须和类名称一模一样 一定都不能有一点区别
      2.构造方法不要写返回值类型，连void都不能有 构造器唯一能用的修饰符 就是一个public
-
+     3.没有编写构造方法时，编译器会自动创建一个空的构造方法，这个方法是什么都没做 空的，当我们自己定义了相应的构造方法 原来的方法就会消失
+     4.构造方法也是可以重载的 和一般的方法都是一样 可以重载 overload
 作用：
     1.在创建对象之际就能给对象赋初值；
     2.
@@ -37,6 +38,11 @@ public class Student {
         setAge(18);
         setName("Taylor Swift");
         System.out.println("我刚刚用了构造器创建了对象 而且我还给对象设置了初值");
+        this.Name="Taylor Swift2号选手 来自于this关键字创建的新名字";
+    }
+    public Student(int age,String Name){
+        this.age=age;
+        this.Name=Name;
     }
 }
 
--- day09-oop/src/come/atsue/java/StudentUse.java
+++ day09-oop/src/come/atsue/java/StudentUse.java
@@ -2,7 +2,12 @@ package come.atsue.java;
 
 public class StudentUse {
     public static void main(String[] args) {
-        Student stu=new Student();
+        Student stu=new Student();//无参构造器创建的对象
         System.out.println("我叫"+stu.getName()+",很高兴遇到你们，我今年"+stu.getAge()+"岁了");
+        Student stu2=new Student(19,"TaylorSwift三号 我是来自己于从重载之后的构造方法");
+//                                  有参构造器创建的对象 在创建对象可以调用方法 这个时候就可以不断地创造新
+//                                  的对象，而无参构造器却都是创建的一样的对象
+//                                  不要忘了 我们的setget方法任然是有用的 因为一个对象的属性是会变的
+        System.out.println("我叫"+stu2.getName()+",很高兴遇到你们，我今年"+stu2.getAge()+"岁了");
     }
 }
