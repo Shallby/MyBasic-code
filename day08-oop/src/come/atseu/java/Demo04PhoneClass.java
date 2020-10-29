7570a131ed3d7cdacee56c2dbb306a1c8b5bd43e
Shallby <1012437497@qq.com>
2020-10-29 12:06:37 +0800

    Review how to creat a basic class

---
+++ day08-oop/src/come/atseu/java/Demo04PhoneClass.java
@@ -0,0 +1,15 @@
+package come.atseu.java;
+/*
+定义一个手机类
+属性：品牌 价格 颜色
+行为 打电话 发短信
+ */
+public class Demo04PhoneClass {
+    String brand;
+    int price;
+
+
+    public void call(){
+        System.out.println(brand+"手机可以打电话");
+    }
+}
