c7b3d3fe87a7810d145240d02686a8aaf9060484
Shallby <1012437497@qq.com>
2020-10-29 15:06:34 +0800

    Using the constructor we defined,should be careful with the general method.cause of the construct method doesn't have return,So we shouldn't use a parameter to get the result of construction function.

---
+++ day09-oop/src/come/atsue/java/StudentUse.java
@@ -0,0 +1,8 @@
+package come.atsue.java;
+
+public class StudentUse {
+    public static void main(String[] args) {
+        Student stu=new Student();
+        System.out.println("我叫"+stu.getName()+",很高兴遇到你们，我今年"+stu.getAge()+"岁了");
+    }
+}
