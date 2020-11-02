4b0514cf21555480ec5d69c81f9f885318a73be9
Shallby <1012437497@qq.com>
2020-11-02 21:26:49 +0800

    Mainly about the Inherited ,abtract,Arraylist<E>,Random()Class,this getter and setter method.

--- day12-Inherited/src/DemoHongBao/Qunzhu.java
+++ day12-Inherited/src/DemoHongBao/Qunzhu.java
@@ -1,11 +1,13 @@
 package DemoHongBao;
-
 import java.util.ArrayList;
 
 /*
 由于抽象类有两种构造方法 所以子类 也就是这个类至少得有一个包含参数的构造器 去调用父类/抽象类中的有参构造器
  */
 public class Qunzhu extends Uer{
+    //虽然是继承了了抽象类的构造器 但是子类本身也要有相应的构造器 这样子才能有this 以供后面使用
+    //否则那么只能使用super.关键字来创建对象  总之 这个必须的 就是至少子类也得包括父类的构造器 简单的做法是在子类的构造器中调用
+    //父类的super（参数）构造器
     public Qunzhu(String name, int balance) {
         super(name, balance);
     }
